7243054e36f549bfeca68ef6f293d3316eaf69cb
马艳绿 马 <2040266560@qq.com>
2026-09-19 12:33:42 +0800

    新增领地圈地教程文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,6 @@ import { resolve } from 'path'
 import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config'
 import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
 import dayjs from 'dayjs'
-import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 
 const DOMAIN_NAME = 'docs.freecore.cc' // 域名 (不带https)
